899e37e48863c456618328d977ca5179abd9765b
GING01 <andres.paredes@epn.edu.ec>
2018-08-30 11:24:51 -0500

    Crear studiante activity

---
+++ app/src/main/java/mta/epn/ginghogam/com/mitaller/activities/EstudiantesActivity.java
@@ -0,0 +1,15 @@
+package mta.epn.ginghogam.com.mitaller.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mta.epn.ginghogam.com.mitaller.R;
+
+public class EstudiantesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estudiantes);
+    }
+}
