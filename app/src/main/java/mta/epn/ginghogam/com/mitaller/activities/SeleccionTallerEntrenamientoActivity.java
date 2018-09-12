75aca422a66231ce1182ee63fbc47497969d718e
CarlosEd91 <carlos.e.sampedro.91@gmail.com>
2018-09-12 17:48:54 -0500

    Texto a voz

--- app/src/main/java/mta/epn/ginghogam/com/mitaller/activities/SeleccionTallerEntrenamientoActivity.java
+++ app/src/main/java/mta/epn/ginghogam/com/mitaller/activities/SeleccionTallerEntrenamientoActivity.java
@@ -3,16 +3,22 @@ package mta.epn.ginghogam.com.mitaller.activities;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Build;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.RequiresApi;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import mta.epn.ginghogam.com.mitaller.R;
 
@@ -24,7 +30,9 @@ import mta.epn.ginghogam.com.mitaller.entidades.Taller;
 import mta.epn.ginghogam.com.mitaller.entidades.Tutor;
 import mta.epn.ginghogam.com.mitaller.utilidades.ShadowTransformer;
 
-public class SeleccionTallerEntrenamientoActivity extends AppCompatActivity {
+import static android.graphics.Color.rgb;
+
+public class SeleccionTallerEntrenamientoActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     private ViewPager mViewPager;
     private CardPagerAdapterS mCardAdapter;
@@ -38,7 +46,13 @@ public class SeleccionTallerEntrenamientoActivity extends AppCompatActivity {
     private Estudiante estudiante;
     private Tutor tutor;
 
-    private TextView texto;
+    private TextView lectura;
+    private ImageView guia;
+
+    private TextToSpeech TtS;
+    private Handler mHandler;
+    private Runnable mRunnable;
+    private int i = 0;
 
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -46,12 +60,13 @@ public class SeleccionTallerEntrenamientoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_data);
+        TtS=new TextToSpeech(this,this);
 
         Bundle extras = getIntent().getExtras();
         tutor = extras.getParcelable("tutor");
         estudiante = extras.getParcelable("estudiante");
 
-        Toast.makeText(this,"Estudiante: "+estudiante.getNombreEstudiate()+ " - Tutor: "+tutor.getNombreTutor(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Estudiante: " + estudiante.getNombreEstudiate() + " - Tutor: " + tutor.getNombreTutor(), Toast.LENGTH_LONG).show();
 
 
         context = this;
@@ -79,17 +94,76 @@ public class SeleccionTallerEntrenamientoActivity extends AppCompatActivity {
                 taller.setImagenTaller(cursor.getString(3));
                 tallerList.add(taller);
                 mCardAdapter.addCardItemS(taller);
-            }while (cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
 
-        texto = findViewById(R.id.texto);
+        lectura = findViewById(R.id.texto);
+        guia = findViewById(R.id.guia);
 
-        texto.setText(tutor.getNombreTutor());
+        String msj = "Variación de una magnitud en función de la distancia, " +
+                "a partir de la línea en que esta variación es máxima en las magnitudes cuyo valor es distinto en los diversos puntos de una región del espacio.";
+        lectura.setText(msj);
+        lectura.setTextColor(rgb(255,192,0));
+
+        guia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hablar();
+            }
+        });
 
         mCardShadowTransformer = new ShadowTransformer(mViewPager, mCardAdapter);
         mViewPager.setAdapter(mCardAdapter);
         mViewPager.setPageTransformer(false, mCardShadowTransformer);
         mViewPager.setOffscreenPageLimit(3);
+
+    }
+
+    private void hablar() {
+        String msj = "Un gradiente es la variación de una magnitud en función de la distancia, " +
+                "a partir de la línea en que esta variación es máxima en las magnitudes cuyo valor es distinto en los diversos puntos de una región del espacio." +
+                " Variación de una magnitud en función de la distancia, \" +\n" +
+                "                \"a partir de la línea en que esta variación es máxima en las magnitudes cuyo valor es distinto en los diversos puntos de una región del espacio.";
+        final String texto= msj.toString();
+
+        final String [] palabraResaltada = texto.split("\\s+");
+        lectura.setText("");
+        i=0;
+        final Handler mHandler = new Handler();
+
+        mHandler.post(new Runnable(){
+            @Override
+            public void run() {
+                lectura.append(palabraResaltada[i]+" ");
+                lectura.setTextColor(rgb(255,192,0));
+                lectura.setMovementMethod(new ScrollingMovementMethod());
+
+
+                i++;
+                if(i < palabraResaltada.length) {
+                    mHandler.postDelayed(this, 300);
+
+                }
+            }
+        });
+        TtS.speak(texto, TextToSpeech.QUEUE_FLUSH,null);
+
+
+    }
+
+    @Override
+    public void onInit(int text) {
+        if(text==TextToSpeech.SUCCESS){
+            int lenguaje= TtS.isLanguageAvailable(new Locale("spa", "ESP"));
+            if(lenguaje == TextToSpeech.LANG_MISSING_DATA || lenguaje==TextToSpeech.LANG_NOT_SUPPORTED){
+                guia.setSaveEnabled(true);
+                hablar();
+
+            }
+            else {
+            }
+        }
+        else{}
 
     }
 }
