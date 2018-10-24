7ab274f4772ca05f5fd97cb49e579cebf95f8bd7
CarlosEd91 <carlos.e.sampedro.91@gmail.com>
2018-10-23 22:24:28 -0500

    commit

--- app/src/main/java/mta/epn/ginghogam/com/mitaller/activities/EdicionSecuenciaActivity.java
+++ app/src/main/java/mta/epn/ginghogam/com/mitaller/activities/EdicionSecuenciaActivity.java
@@ -9,6 +9,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -51,7 +52,7 @@ public class EdicionSecuenciaActivity extends AppCompatActivity {
     private LinearLayout rootLayout, target;
 
     ImageView bt1, imagen, camara, galery;
-    EditText descripcionImagenSecuencia;
+    EditText descripcionImagenSecuencia, sequenceDes;
     Integer numeroImg;
     private File fileImagen;
     private String pathCamara, pathGaleria, pathArrastrar;
@@ -122,12 +123,27 @@ public class EdicionSecuenciaActivity extends AppCompatActivity {
             } while (cursor.moveToNext());
             for (int i = 0; i < secuenciaList.size(); i++) {
                 bt1 = new ImageView(getApplicationContext());
+                sequenceDes = new EditText(getApplicationContext());
 
                 LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(180, 180);
                 parametros.gravity = Gravity.CENTER;
                 parametros.setMargins(10, 10, 10, 10);
 
-                bt1.setLayoutParams(parametros);
+                LinearLayout.LayoutParams parametros2 = new LinearLayout.LayoutParams(180, 180);
+                parametros.gravity = Gravity.CENTER;
+
+                bt1.setLayoutParams(parametros2);
+                sequenceDes.setLayoutParams(parametros);
+                sequenceDes.setText(secuenciaList.get(i).getDescripcionImagenSecuencia());
+//                sequenceDes.setBackgroundColor(Color.WHITE);
+//                sequenceDes.setTextColor(Color.BLACK);
+//                GradientDrawable border = new GradientDrawable();
+//                border.setColor(Color.WHITE); //white background
+//                border.setStroke(1, Color.BLACK); //black border with full opacity
+//                border.setCornerRadius(1);
+//                sequenceDes.setBackground(border);
+                sequenceDes.setPadding(10,10,10,10);
+
 
 
                 bt1.setImageBitmap(BitmapFactory.decodeFile(secuenciaList.get(i).getImagenSecuencia()));
@@ -135,6 +151,7 @@ public class EdicionSecuenciaActivity extends AppCompatActivity {
                 bt1.setOnDragListener(dragListener);
                 bt1.setTag((secuenciaList.get(i).getOrdenImagenSecuencia()));
                 rootLayout.addView(bt1);
+                rootLayout.addView(sequenceDes);
 
 
                 final int finalI = i;
